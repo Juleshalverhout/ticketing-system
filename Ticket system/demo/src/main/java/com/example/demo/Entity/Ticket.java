952a0881b4f79fa8ac5a4bb17944e53323721f92
halverhoutjules <halverhout.jules@gmail.com>
2026-04-21 15:10:29 +0200

    Initial commit

---
+++ Ticket system/demo/src/main/java/com/example/demo/Entity/Ticket.java
@@ -0,0 +1,60 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+
+    public Ticket() {
+
+    }
+
+    public Ticket(String title, String description, String status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
