118e563e5d2a7665caae792123c5a64ad9c65225
halverhoutjules <halverhout.jules@gmail.com>
2026-06-10 10:30:54 +0200

    Created User Entity and UserRepository

---
+++ Ticket system/demo/src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,55 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+
+    public User(Long id, String username, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
